6528de33202841bef6618ff637dd48881e17497e
ruhm <ruhm.st@gmail.com>
2016-05-03 14:33:48 +0800

    updating hugo dir 2016-05-03 14:33:48

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,9 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
+
+curl -X POST --data-urlencode 'payload={"channel": "#blogsite", "username": "webhookbot", "text": "rebuilding site"}' https://hooks.slack.com/services/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
+
 # Come Back
 cd ..
 
@@ -29,8 +32,9 @@ echo -e "\033[0;32mCommit hugo dir content updates to coding...\033[0m"
 # Add changes to git.
 git add -A
 
+updateTime=$(date '+%Y-%m-%d %H:%M:%S')
 # Commit changes.
-msg="updating hugo dir $(date '+%Y-%m-%d %H:%M:%S')"
+msg="updating hugo dir $updateTime"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
@@ -38,6 +42,8 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
+
+curl -X POST --data-urlencode 'payload={"channel": "#blogsite", "username": "webhookbot", "text": "updating hugo directory"}' https://hooks.slack.com/services/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
 
 # Come Back
 cd ..
